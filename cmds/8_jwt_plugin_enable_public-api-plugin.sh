7d231c6bf47fc898cfa952072a7728517cedc2bf
boburumurzokov <borbur.umurzokov@astrazeneca.com>
2022-04-09 10:54:41 +0300

    add public-ip tutorial for JWT plugin

---
+++ cmds/8_jwt_plugin_enable_public-api-plugin.sh
@@ -0,0 +1,7 @@
+curl http://127.0.0.1:9080/apisix/admin/routes/jas -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+    "uri": "/apisix/plugin/jwt/sign",
+    "plugins": {
+        "public-api": {}
+    }
+}'
